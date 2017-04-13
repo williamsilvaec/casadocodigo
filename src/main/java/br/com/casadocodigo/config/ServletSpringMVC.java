b132cc377138d8686e207e4e94956b08af60e978
william <williamsousadasilva@gmail.com>
2017-04-13 16:39:33 -0300

    versão inicial, configurações do spring e as dependências do projeto.

---
+++ src/main/java/br/com/casadocodigo/config/ServletSpringMVC.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {AppWebConfiguration.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
